4d90b53029d696163000919101948f67b54e7941
serhatprogramming <serhatprogramming@gmail.com>
2023-04-17 17:51:09 -0400

    initial form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>Test</div>;
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+  const [height, setHeight] = useState("");
+
+  return (
+    <div>
+      <form>
+        name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        birtdate:
+        <input
+          type="date"
+          value={born}
+          onChange={(e) => setBorn(e.target.value)}
+        />
+        height:{" "}
+        <input
+          type="number"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+        />
+      </form>
+      <div>
+        {name} was born on {born} with height of
+        {height}
+      </div>
+    </div>
+  );
 };
 
 export default App;
